c79012ccb08f1f04affedcca964d240c3c467d6e
August <augustgosden@gmail.com>
2026-02-10 14:26:43 +0100

    first addition

---
+++ src/main/java/org/example/spaceportalbe/SpacePortalBeApplication.java
@@ -0,0 +1,13 @@
+package org.example.spaceportalbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpacePortalBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpacePortalBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/spaceportalbe/SpacePortalBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.spaceportalbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpacePortalBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
